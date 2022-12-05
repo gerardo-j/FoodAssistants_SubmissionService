bcaabf97bec58d4b8f897e09e40d93a91494aa4d
Carlos German <carlosgerman353@gmail.com>
2022-12-05 16:49:14 -0500

    add some details

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SubmissionModule } from './submission/submission.module';
 
+// intial 
 @Module({
   imports: [SubmissionModule],
   controllers: [AppController],
