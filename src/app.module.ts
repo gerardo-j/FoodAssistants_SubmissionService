c97b0f732867e00255269d87656a8d216783c40b
Gerardo <gerardojuarez695@gmail.com>
2022-12-04 22:37:23 -0500

    Remove submission controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SubmissionController } from './submission/submission.controller';
 import { SubmissionModule } from './submission/submission.module';
-import { SubmissionService } from './submission/submission.service';
 
 @Module({
   imports: [SubmissionModule],
-  controllers: [AppController, SubmissionController],
-  providers: [AppService, SubmissionService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
