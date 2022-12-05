13db7f0ee8b3f44a4f94ed52f68bf8b2d75312d6
XJHCODEX <98928177+XJHCODEX@users.noreply.github.com>
2022-12-05 16:16:30 -0500

    Remove ID

--- src/submission/submission.service.ts
+++ src/submission/submission.service.ts
@@ -5,4 +5,10 @@ export class SubmissionService {
   findAll(): any[] {
     return [{}];
   }
+    remove(id:number):void{ /* needs editting */
+    const submission = this.findAll(id);
+    const post = this.submission.indexOf(submission);
+    this.submission.splice(pos, 1);
+    return true;
+  }
 }
