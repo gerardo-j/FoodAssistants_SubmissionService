d0ca4bfee453ca14cbce270d0badc5585b479052
Andy Kan <72168163+andygvd@users.noreply.github.com>
2022-12-13 00:58:24 +0000

    entity class outlining data received

--- src/submission/entity/submission.entity.ts
+++ src/submission/entity/submission.entity.ts
@@ -4,4 +4,26 @@ import { Entity, PrimaryGeneratedColumn } from 'typeorm';
 export class SubmissionEntity {
   @PrimaryGeneratedColumn({ name: 'id' })
   id: number;
+    name: string;
+    phone: string;
+    address: string;
+    distribution: string;
+    residents: number;
+    items: {
+        grocery: string[];
+        generalHygiene: string[];
+        feminineHygiene: string[];
+        cleaningAndHealth: string[];
+    }
+
+    constructor(name: string, phone: string, address: string, distribution: string, residents: number, items: 
+        {grocery: string[]; generalHygiene: string[]; feminineHygiene: string[]; cleaningAndHealth: string[];}) {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.distribution = distribution;
+        this.residents = residents;
+        this.items = items;
+       }
 }
+
